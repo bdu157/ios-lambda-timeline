329ba9175b3d70fee6bcd652250a2af80803ad73
Dongwoo Pae <dongwoopae@gmail.com>
2019-09-19 14:05:05 +0900

    added CIColorControls with brightness, contrast and saturation

--- LambdaTimeline/View Controllers/ImagePostViewController.swift
+++ LambdaTimeline/View Controllers/ImagePostViewController.swift
@@ -11,6 +11,48 @@ import Photos
 
 class ImagePostViewController: ShiftableViewController {
     
+    var postController: PostController!
+    var post: Post?
+    var imageData: Data?
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var chooseImageButton: UIButton!
+    @IBOutlet weak var imageHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var postButton: UIBarButtonItem!
+    
+    //slider outlets
+    @IBOutlet weak var brightnessSlider: UISlider!
+    @IBOutlet weak var contrastSlider: UISlider!
+    @IBOutlet weak var saturationSlider: UISlider!
+    
+    //taking original image to make it smaller and easier to manupulate while making changes for its preview
+    var originalimage: UIImage? {
+        didSet {
+            guard let originalImage = self.originalimage else {return} //once we selected image we run it
+            
+            //height and width
+            var scaledSize = imageView.bounds.size
+            
+            // 1x, 2x, 3x(10x max)
+            let scale = UIScreen.main.scale  //screen size based on user's device
+            
+            scaledSize = CGSize(width: scaledSize.width * scale, height: scaledSize.height * scale)
+            
+            self.scaledImage = originalImage.imageByScaling(toSize: scaledSize) //when originalimage gets available then make scaledImage
+        }
+    }
+    
+    var scaledImage: UIImage? {
+        didSet {
+            self.updateImage()
+        }
+    }
+    
+    private let context = CIContext(options: nil)
+    private let filter = CIFilter(name: "CIColorControls")!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -48,7 +90,7 @@ class ImagePostViewController: ShiftableViewController {
         imagePicker.delegate = self
         
         imagePicker.sourceType = .photoLibrary
-
+        
         present(imagePicker, animated: true, completion: nil)
     }
     
@@ -58,8 +100,8 @@ class ImagePostViewController: ShiftableViewController {
         
         guard let imageData = imageView.image?.jpegData(compressionQuality: 0.1),
             let title = titleTextField.text, title != "" else {
-            presentInformationalAlertController(title: "Uh-oh", message: "Make sure that you add a photo and a caption before posting.")
-            return
+                presentInformationalAlertController(title: "Uh-oh", message: "Make sure that you add a photo and a caption before posting.")
+                return
         }
         
         postController.createPost(with: title, ofType: .image, mediaData: imageData, ratio: imageView.image?.ratio) { (success) in
@@ -112,28 +154,65 @@ class ImagePostViewController: ShiftableViewController {
         view.layoutSubviews()
     }
     
-    var postController: PostController!
-    var post: Post?
-    var imageData: Data?
     
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var titleTextField: UITextField!
-    @IBOutlet weak var chooseImageButton: UIButton!
-    @IBOutlet weak var imageHeightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var postButton: UIBarButtonItem!
+    // MARK: - Slider actions
+    
+    @IBAction func brightnessChanged(_ sender: Any) {
+        self.updateImage()
+    }
+    
+    @IBAction func contrastChanged(_ sender: Any) {
+        self.updateImage()
+    }
+    
+    @IBAction func saturationChanged(_ sender: Any) {
+        self.updateImage()
+    }
+    
+    private func updateImage() {
+        if let scaledImage = self.scaledImage {
+            imageView.image = self.image(byFiltering: scaledImage)
+        } else {
+            imageView.image = nil
+        }
+    }
+    
+    private func image(byFiltering image: UIImage) -> UIImage {
+        
+        //this will take scaledImage
+        
+        //uiimage -> cgimage -> ciimage
+        guard let cgImage = image.cgImage else {return image}  //cgImage can be used from UIImage
+        let ciImage = CIImage(cgImage: cgImage)
+        
+        //Set the values of the filter's paremeters
+        filter.setValue(ciImage, forKey: "inputImage")  //take the image
+        filter.setValue(saturationSlider.value, forKey: "inputSaturation")  //set the saturationSlider to have inputSaturation
+        filter.setValue(brightnessSlider.value, forKey: "inputBrightness") // set the brightnessSlider to have inputBrightness
+        filter.setValue(contrastSlider.value, forKey: "inputContrast") // set the contrastSlider to hvae inputContrast
+        
+        // the metadata to be processed. not the actual filtered image
+        //ciimage -> cgimage -> uiimage
+        guard let outputCIImage = filter.outputImage else {return image}
+        guard let outputCGImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) else {return image}
+        return UIImage(cgImage: outputCGImage)
+    }
 }
+
+
 
 extension ImagePostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-
+        
         chooseImageButton.setTitle("", for: [])
         
         picker.dismiss(animated: true, completion: nil)
         
         guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
         
-        imageView.image = image
+        //imageView.image = image
+        self.originalimage = image
         
         setImageViewHeight(with: image.ratio)
     }
@@ -141,4 +220,9 @@ extension ImagePostViewController: UIImagePickerControllerDelegate, UINavigation
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
+    
+    
+    //slider actions
+    
+    
 }
