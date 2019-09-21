b3431cd69d535ce5d4d9c48e8795e0a3a9e3dd3b
Dongwoo Pae <dongwoopae@gmail.com>
2019-09-21 17:08:45 +0900

    added UI for recording comments

--- LambdaTimeline/View Controllers/AudioCommentViewController.swift
+++ LambdaTimeline/View Controllers/AudioCommentViewController.swift
@@ -7,16 +7,111 @@
 //
 
 import UIKit
+import AVFoundation
 
-class AudioCommentViewController: UIViewController {
+class AudioCommentViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
+    //MARK: RECORDING
+    
+    private var recorder: AVAudioRecorder?
+    
+    private var recordingURL: URL?
+    
+    private var isRecording: Bool {
+        return recorder?.isRecording ?? false
+    }
+    
+    @IBAction func recordButtonTapped(_ sender: Any) {
+        defer {updateButtons()}
+        
+        guard !isRecording else {
+            recorder?.stop()
+            return
+        }
+        
+        do {
+            let format = AVAudioFormat(standardFormatWithSampleRate: 44100.0, channels: 2)!
 
-    /*
+            recorder = try AVAudioRecorder(url: self.newRecordingURL(), format: format)
+            recorder?.delegate = self
+            recorder?.record()
+        } catch {
+            NSLog("Unable to start recording: \(error)")
+        }
+    }
+    
+    private func newRecordingURL() -> URL {
+        let fm = FileManager.default
+        let documentsDirectory = try! fm.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        return documentsDirectory.appendingPathComponent("caf")
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        self.recordingURL = recorder.url
+        self.recorder = nil
+        self.updateButtons()
+    }
+    
+    
+    //MARK: PLAYING
+    private var player: AVAudioPlayer?
+    
+    private var isPlaying: Bool {
+        return player?.isPlaying ?? false
+    }
+    
+    @IBAction func playRecordedFile(_ sender: Any) {
+        defer{updateButtons()}
+        
+        guard let url = self.recordingURL else {return}
+        
+        guard !isPlaying else {
+            player?.stop()
+            return
+        }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.delegate = self
+            player?.play()
+        } catch {
+            NSLog("Unable to start playing: \(error)")
+        }
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        self.player = nil
+        self.updateButtons()
+    }
+    
+    
+    //MARK: CANCEL
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    //updateButtons for both of recording and playing
+    private func updateButtons() {
+        let playButtonString = self.isPlaying ? "Stop Playing" : "Play"
+        self.sendButton.setTitle(playButtonString, for: .normal)
+        
+        let recordButtonString = self.isRecording ? "Stop Recording" : "Record"
+        self.recordButton.setTitle(recordButtonString, for: .normal)
+    }
+    
+    
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -24,6 +119,5 @@ class AudioCommentViewController: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
-
+    
 }
